70125985f49e7ec330a4a4e87d54a7b803ceabe8
Muhammad Rafli <44805762+RabbitCL4Y@users.noreply.github.com>
2019-01-23 20:55:06 +0800

    Create Hp.sh

---
+++ Hp.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#Author = 085752481601
+#Contact = RabbitCL4Y123@gmail.com
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+figlet -f future PLEASE WAIT!!! |lolcat
+sleep 2
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+figlet -f slant MR.CL4Y |lolcat
+sleep 2
+echo "***********************RCT*********************************"|lolcat
+echo "Author By   : MR.CL4Y"|lolcat
+echo "My Team     : Rabbit Cyber Team"|lolcat
+echo "Contact Me  : RabbitCL4Y123@gmail.com"|lolcat
+echo "My GitHub   : https://github.com/RabbitCL4Y"|lolcat
+echo "My GitLab   : https://gitlab.com/MRCL4Y"|lolcat
+echo "***********************RCT*********************************"|lolcat
+echo "**********LACAK IP LEWAT NOMOR HP**********"
+echo "Y"
+echo "T"
+echo "*******************************************"
+read -p "Root@MR.CL4Y": Put
+
+if [ $Put = Y ] || [ $put = y ]
+then
+clear
+figlet -f slant LACAK IP |lolcat
+sleep 1
+clear
+xdg-open 'http://www.ceebydith.com/cek-hlr-lokasi-hp.html'
+fi
+
+if [ $Put = T ] || [ $Put = t ]
+then
+clear
+figlet -f slant MATI AJA LHU |lolcat
+sleep 1
+clear
+exit
+fi
